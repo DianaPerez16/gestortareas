40a4512bfe8d5a945d25e41ddc8785ebca48e05a
Diana Perez <diana.prza16@gmail.com>
2024-08-06 19:01:20 -0600

    Compilación con webpack, añade tareas

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development', // Para saber si esta en desarrollo o producción
     entry: './src/index.js', //punto de entrada de la aplicación
     output: {
        filename: 'bundle.js', //nombre del archivo de salida
        path: path.resolve(__dirname, 'dist'), //carpeta de salida
     },
     module: {
-        rule: [
+        rules: [
             {
                 test: /\.css$/,  //Regex para identificar archivos css
                 use: ['style-loader', 'css-loader'], //Loader para librerías css
@@ -26,7 +27,9 @@ module.exports = {
     },
     devtool: 'source-map', //Genera source maps para facilitar la depuracion
     devServer: {
-        contentBase: path.resolve(__dirname, 'dist'), //Carpeta que va a correr el servidor
+        static: {
+            directory: path.resolve(__dirname, 'dist'), //Carpeta del que correrá el servidor
+        },
         compress: true, //Habilitar compresión gzip 
         port: 9000, //Puerto del servidor de desarrollo 
     },
